1281200df5411a5384d797492aa68066a4e11c73
Denis Woldemariam <damewold@gmail.com>
2020-04-05 19:06:48 -0500

    inserted images

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'react'
 
 class App extends Component {
+
+constructor(){
+  super()
+  this.state = {
+    galleryList:[],
+    galleryItem:{
+      path:'path',
+     description:'description',
+     like:'likes'
+    }
+  }
+}
+
+componentDidMount(){
+  this.getGalleryItem();
+}
+
+getGalleryItem = () => {
+  axios.get('/gallery')
+    .then( response => {
+      this.setState({
+        galleryList: response.data
+      })
+    })
+    .catch( error => {
+      alert(`Couldn't get inventory. Try again later`);
+      console.log('Error ', error);
+    })
+}
+
+
+
+
+
+
+
+  
   render() {
     return (
       <div className="App">
@@ -9,8 +47,14 @@ class App extends Component {
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br/>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <p>Gallery</p>
+       
+        <ul>
+{this.state.galleryList.map(song =>
+<li >
+{galleryList.id}</li>)}
+</ul> 
+        
       </div>
     );
   }
