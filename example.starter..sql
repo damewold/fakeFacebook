27b17a44b7f4e2323cd528f74d5699cfcba2e325
Denis Woldemariam <damewold@gmail.com>
2020-09-02 15:38:16 -0500

    renamed the folder and made changese to the db

---
+++ example.starter..sql
@@ -0,0 +1,14 @@
+CREATE TABLE "gallery" (
+	"id" SERIAL PRIMARY KEY,
+	"path" VARCHAR(350) NOT NULL,
+	"description" VARCHAR (250) NOT NULL,
+	"likes" INTEGER NOT NULL
+);
+
+-- INSERT INTO "example-starter" ()
+-- VALUES ()
+
+
+
+ 
+    
